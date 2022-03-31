9adb44297259cf389fe9a38ee5b075f384fd33e6
hassan-harera <hassan.shaban.harera@gmail.com>
2022-03-31 16:06:06 +0200

    select service

---
+++ features/select-seat/src/main/java/com/englizya/select_seat/SelectSeatFragment.kt
@@ -0,0 +1,35 @@
+package com.englizya.select_seat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.englizya.common.base.BaseFragment
+import com.englizya.select_seat.databinding.FragmentSelecetSeatBinding
+
+class SelectSeatFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentSelecetSeatBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSelecetSeatBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
---
+++ features/select-station/src/main/java/com/englizya/select_station/SelectStationFragment.kt
@@ -0,0 +1,35 @@
+package com.englizya.select_station
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.englizya.common.base.BaseFragment
+import com.englizya.select_station.databinding.FragmentSelectStationBinding
+
+class SelectStationFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentSelectStationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSelectStationBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
