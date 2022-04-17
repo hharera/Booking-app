f1ba28a4132fdefd7ee615d394691e44b65c598d
hassan-harera <hassan.shaban.harera@gmail.com>
2022-04-17 15:25:07 +0200

    start testing

--- data/remote/api/src/androidTest/java/com/englizya/api/impl/ReservationServiceImplTest.kt
+++ data/remote/api/src/androidTest/java/com/englizya/api/impl/ReservationServiceImplTest.kt
@@ -1,18 +1,43 @@
 package com.englizya.api.impl
 
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import org.junit.Rule
+import com.englizya.api.TripService
+import com.englizya.api.di.clientModule
+import com.englizya.api.di.remoteModule
+import com.englizya.model.request.TripSearchRequest
+import io.ktor.util.*
+import org.joda.time.DateTime
+import org.junit.Before
 import org.junit.Test
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
 
-@HiltAndroidTest
-class ReservationServiceImplTest {
+@InternalAPI
+class ReservationServiceImplTest : KoinTest {
 
-    @get:Rule
-    var hiltRule = HiltAndroidRule(this)
+    private val tripService: TripService by inject()
+
+    @Before
+    fun setup() {
+        startKoin {
+            loadKoinModules(
+                arrayListOf(
+                    clientModule,
+                    remoteModule
+                )
+            )
+        }
+    }
 
     @Test
-    fun test(): Unit {
-
+    suspend fun test(): Unit {
+        tripService.searchTrips(
+            TripSearchRequest(
+                DateTime.now().toString(),
+                1,
+                2
+            )
+        )
     }
 }
