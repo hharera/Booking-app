c5e0874981d828a7b576a55fed0f34b9d2ab6938
hassan-harera <hassan.shaban.harera@gmail.com>
2022-04-25 22:50:38 +0200

    release 0.3.0 reset password

--- data/remote/api/src/main/java/com/englizya/api/utils/Constants.kt
+++ data/remote/api/src/main/java/com/englizya/api/utils/Constants.kt
@@ -42,7 +42,3 @@ object Constants {
     const val TOKEN = "token"
 }
 
-object Domain {
-//        const val ENGLIZYA_PAY = "http://161.97.71.140:9100/"
-    const val ENGLIZYA_PAY = "http://192.168.1.190:9100/"
-}
