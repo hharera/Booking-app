48c8c0d01486c26324235dfb776b2795dd2cca52
hassan-harera <hassan.shaban.harera@gmail.com>
2022-05-23 14:42:51 +0200

    change refund policy

--- features/terms-conditions/src/main/java/com/englizya/terms_conditions/utils/RefundPolicy.kt
+++ features/terms-conditions/src/main/java/com/englizya/terms_conditions/utils/RefundPolicy.kt
@@ -2,13 +2,13 @@ package com.englizya.terms_conditions.utils
 
 object RefundPolicy {
 
-    const val ARABIC =  "\u2022 قبل ميعاد الرحله ب 27 ساعه بدون قيمه\n" +
-            "\u2022 قبل ميعاد الرحله ب 84 ساعه خصم 5 %من قيمة التذكره\n" +
-            "\u2022 قبل ميعاد الرحله ب 78 ساعه 01 % من قيمة التذكره\n" +
-            "\u2022 قبل ميعاد الرحله 8 ساعات 51 % من قيمة التذكره\n" +
+    const val ARABIC =  "\u2022 قبل ميعاد الرحله ب 24 ساعه بدون قيمه\n" +
+            "\u2022 قبل ميعاد الرحله ب 48 ساعه خصم 5 %من قيمة التذكره\n" +
+            "\u2022 قبل ميعاد الرحله ب 72 ساعه 01 % من قيمة التذكره\n" +
+            "\u2022 قبل ميعاد الرحله 4 ساعات 51 % من قيمة التذكره\n" +
             "\u2022 عد ذلك من الاوقات المسموحه ال يجوز الاسترجاع\n" + 
             "وهذه الاحكام تسري وفقا لطرق الاسترجاع من خالل الشباك وقد تسري ايضا هذه السياسات وفقا لقانون\n" +
-            "الاستثمار وفقا الحكام المواد ) 63 و 62 ) بشأن ما تم دفعه اليكترونيا يسترجع اليكترونيا خالل مدة 08\n" +
+            "الاستثمار وفقا الحكام المواد ) 37 و 36 ) بشأن ما تم دفعه اليكترونيا يسترجع اليكترونيا خالل مدة 14\n" +
             "يوم غير شاملة العطالت والاجازات الرسميه وال تسري هذه البنود في حال مغادرة الرحله وتخلف الراكب\n" +
             "عن المواعيد المدونه علي التذكره او الموقع او تطبيق الحجز الاليكتروني\n" +
             "وقد ال يسمح باي انواع الاسترجاع في الاعياد والمناسبات وفقا لسيسات الوقت الراهن بشأن ضغط الاركاب\n" +
@@ -16,13 +16,13 @@ object RefundPolicy {
             "ال يجوز نقل الكراسي من خط الي خط اخر ويسمح بالنقل علي نفس الخط بنفس المدينة وقد يسمح بالنقل\n" +
             "قبل ميعاد الرحله بساعتين وفقا الحكام وسياسات الشركه .\n"
 
-    const val ENGLISH = "\u2022 27 hours before the flight, no value\n" +
-            "\u2022 84 hours before the flight, a discount of 5% of the ticket value\n" +
-            "\u2022 78 hours before the flight, 01% of the ticket value\n" +
-            "\u2022 8 hours before the flight, 51% of the ticket value\n" +
+    const val ENGLISH = "\u2022 24 hours before the flight, no value\n" +
+            "\u2022 48 hours before the flight, a discount of 5% of the ticket value\n" +
+            "\u2022 72 hours before the flight, 01% of the ticket value\n" +
+            "\u2022 4 hours before the flight, 51% of the ticket value\n" +
             "\u2022 This is one of the allowed times, and it is not permissible to return\n" +
             "These provisions apply according to the recovery methods through the net, and these policies may also apply according to the law\n" +
-            "The investment in accordance with the provisions of Articles (63 and 62) regarding what was paid electronically, will be returned electronically within a period of 08\n" +
+            "The investment in accordance with the provisions of Articles (36 and 37) regarding what was paid electronically, will be returned electronically within a period of 14\n" +
             "A day that does not include holidays and official holidays, and these provisions do not apply in the event that the flight departs and the passenger is late\n" +
             "About the dates listed on the ticket, the website, or the online booking application\n" +
             "Any types of returns may not be allowed on holidays and occasions, according to the current policies regarding passenger pressure\n" +
