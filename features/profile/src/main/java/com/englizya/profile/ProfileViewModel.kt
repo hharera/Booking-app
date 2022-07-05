2e4fb25e31ea344d7d2bad03e2b48da350c4e3e6
emankhaled96 <emank5501@gmail.com>
2022-07-05 13:30:57 +0200

    adding swiping refresh layout to profile

--- features/profile/src/main/java/com/englizya/profile/ProfileViewModel.kt
+++ features/profile/src/main/java/com/englizya/profile/ProfileViewModel.kt
@@ -31,7 +31,7 @@ class ProfileViewModel constructor(
         fetchUser()
     }
 
-    private fun fetchUser() = viewModelScope.launch(Dispatchers.IO) {
+     fun fetchUser() = viewModelScope.launch(Dispatchers.IO) {
         updateLoading(true)
         userRepository
             .getUser(dataStore.getToken(),false)
@@ -45,7 +45,7 @@ class ProfileViewModel constructor(
             }
     }
 
-    private fun getUserBalance() = viewModelScope.launch(Dispatchers.IO) {
+     fun getUserBalance() = viewModelScope.launch(Dispatchers.IO) {
         walletRepository
             .getBalance(dataStore.getToken())
             .onSuccess {
