a50231be91a5df952e25b7b5c855f28995daba61
emankhaled96 <emank5501@gmail.com>
2022-07-06 11:35:04 +0200

    change domain to remote

--- data/remote/api/src/main/java/com/englizya/api/utils/Domain.kt
+++ data/remote/api/src/main/java/com/englizya/api/utils/Domain.kt
@@ -1,9 +1,9 @@
 package com.englizya.api.utils
 
 object Domain {
-  //  const val ENGLIZYA_BUS_REMOTE = "http://161.97.71.140:9101/"
+    const val ENGLIZYA_BUS_REMOTE = "http://161.97.71.140:9101/"
 
-    const val ENGLIZYA_BUS_REMOTE = "http://192.168.1.190:9101/"
+    // const val ENGLIZYA_BUS_REMOTE = "http://192.168.1.190:9101/"
 
     const val ENGLIZYA_BUS_LOCAL = "http://192.168.1.190:9101/"
 }
