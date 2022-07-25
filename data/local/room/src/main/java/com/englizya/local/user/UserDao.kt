d3c38e74c28a3daa60be60d629e56a12c0774fd5
emankhaled96 <emank5501@gmail.com>
2022-07-25 11:42:37 +0200

    ui updates

---
+++ data/local/room/src/main/java/com/englizya/local/user/UserDao.kt
@@ -0,0 +1,21 @@
+package com.englizya.local.User
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.englizya.model.model.User
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUser(user: User)
+
+    @Query(value = "SELECT * from User ")
+    fun getUsers(): List<User?>
+
+    @Query(value = "SELECT * from User limit 1")
+    fun getUser(): User
+}
