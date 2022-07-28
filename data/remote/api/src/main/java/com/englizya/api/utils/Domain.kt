56761730dc09d456d53dcd99204cdc40b56c3bef
emankhaled96 <emank5501@gmail.com>
2022-07-28 10:02:30 +0200

    update domain

--- data/remote/api/src/main/java/com/englizya/api/utils/Domain.kt
+++ data/remote/api/src/main/java/com/englizya/api/utils/Domain.kt
@@ -1,9 +1,9 @@
 package com.englizya.api.utils
 
 object Domain {
-    const val ENGLIZYA_BUS_REMOTE = "http://ec2-18-223-143-43.us-east-2.compute.amazonaws.com/"
+    const val ENGLIZYA_BUS_REMOTE = "http://161.97.71.140:9101/"
 
-     //const val ENGLIZYA_BUS_REMOTE = "http://ec2-18-223-143-43.us-east-2.compute.amazonaws.com/"
+     //const val ENGLIZYA_BUS_REMOTE = "http://161.97.71.140:9101/"
 
-    const val ENGLIZYA_BUS_LOCAL = "http://ec2-18-223-143-43.us-east-2.compute.amazonaws.com/"
+    const val ENGLIZYA_BUS_LOCAL = "http://192.168.1.190:9101/"
 }
