c05c4fe5ea70a7ab457edb159bfa39ba048b678c
hassan-harera <hassan.shaban.harera@gmail.com>
2022-08-09 14:37:01 +0200

    Merge with development

---
+++ features/user-tickets/src/main/java/com/englizya/user_tickets/utils/PaginationUtils.kt
@@ -0,0 +1,23 @@
+package com.englizya.user_tickets.utils
+
+import com.englizya.model.response.UserTicket
+
+val FakeTicket = UserTicket(
+    "",
+    0,
+    "",
+    "",
+    0,
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    0,
+    false
+)
