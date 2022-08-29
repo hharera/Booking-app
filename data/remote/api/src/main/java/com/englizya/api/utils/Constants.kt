e27464ba98559127fa3bc8353b28904185d78462
EnglizyaIT <it.englizya@gmail.com>
2022-08-29 11:01:26 +0200

    clear data when logout

--- data/remote/api/src/main/java/com/englizya/api/utils/Constants.kt
+++ data/remote/api/src/main/java/com/englizya/api/utils/Constants.kt
@@ -28,7 +28,7 @@ object Routing {
     const val FETCH_USER = "${Domain.ENGLIZYA_BUS_REMOTE}api/profile"
     const val RESET_PASSWORD = "${Domain.ENGLIZYA_BUS_REMOTE}reset-password"
     const val LOGIN = "${Domain.ENGLIZYA_BUS_REMOTE}login"
-    const val EDIT_USER = "${Domain.ENGLIZYA_BUS_REMOTE}api/profile/update"
+    const val EDIT_USER = "${Domain.ENGLIZYA_BUS_REMOTE}api/v1/profile/update"
 
 
     const val GET_ALL_STATIONS = "${Domain.ENGLIZYA_BUS_REMOTE}api/station/all"
